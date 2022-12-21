6779a63c63788d7adb80f911bed84a37c60844d4
Graham Weldon <graham@grahamweldon.com>
2022-12-21 21:03:23 +0900

    Look at me look at me look at me now
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

---
+++ Assets/JGCE/Scripts/MenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace JGCE.Scripts
+{
+    public class MenuController : MonoBehaviour
+    {
+        public void ChangeScene(string scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
